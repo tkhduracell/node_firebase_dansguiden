90688394be33bba706da84dd1d15a4024ab26154
Filip Lindqvist <buggfille@gmail.com>
2017-10-08 16:56:50 +0200

    Fixed version updates

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const pug = require('pug');
 const jf = require('jsonfile');
-
-const admin = require('firebase-admin');
+const scraperjs = require('scraperjs');
+const debug = require('debug')
+const _ = require('lodash/string');
 
 admin.initializeApp(functions.config().firebase);
 
-var db = admin.firestore();
+const db = admin.firestore();
 
 exports.index = functions.https.onRequest((request, response) => {
 	const getVersions = db.collection('versions').get();
@@ -24,13 +26,61 @@ exports.index = functions.https.onRequest((request, response) => {
 			
 			const html = pug.renderFile('views/index.pug', opts);
 			
-			response.status(200).send(html);		
+			response.status(200).send(html);
 		})
 		.catch((err) => {
 			console.error(err);
-			response.status(500).send(err.message);		
+			response.status(500).send(err.message);
 		});
 	
+});
+
+exports.updateVersions = functions.https.onRequest((req, res) => {
+	const log = debug('app:versions');
+	
+	const url = 'https://play.google.com/store/apps/details?id=feality.dans';
+	
+	const extractContent = ($) => {
+		return {
+			lines: $(".whatsnew .recent-change")
+				.map(function () {
+					return $(this).text()
+						.replace(/^\W*\*\W*/, '');
+				})
+				.get(),
+			name: $("div[itemProp='softwareVersion']")
+				.map(function () {
+					return $(this).text()
+						.trim();
+				})
+				.get()
+				.join(", "),
+			date: $("div[itemProp='datePublished']")
+				.map(function () {
+					return $(this).text()
+						.trim();
+				})
+				.get()
+				.join(", ")
+		}
+	}	
+
+	const saveToDb = (data) => {
+		const key = _.snakeCase("v " + data.name);
+		
+		log('Updating ' + key);
+		db.collection('versions').doc(key).set({
+			name: data.name,
+			lines: data.lines
+		}, { merge: true });
+	}
+
+	log('Running Google Play parse');
+	scraperjs.StaticScraper
+		.create(url)
+		.scrape(extractContent, saveToDb);
+
+	res.status(200).send('OK');
 });
 
 function data(elms) {
