a35687697f8a97350d80f53864db3cffe939065f
Filip Lindqvist <buggfille@gmail.com>
2018-08-14 07:43:40 +0200

    Update tests to use async

--- functions/lib/events.test.js
+++ functions/lib/events.test.js
@@ -1,24 +1,22 @@
 const events = require('./events')
 const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
 const _ = require('lodash')
-chai.use(chaiAsPromised)
 
 chai.should()
-
 // node_modules/.bin/mocha --reporter spec functions/lib/events.test.js
 
 describe('events', () => {
   const input = events.update(() => {})
 
-  it('should return none empty array', () => {
-    return input.should.eventually.have.length.greaterThan(1)
+  it('should return none empty array', async () => {
+    const subject = await input
+    subject.should.have.length.greaterThan(1)
   })
 
-  it('should return have no unknowns', () => {
-    const output = input
-      .then(x => x.filter(e => e.type === 'unknown'))
+  it('should return have no unknowns', async () => {
+    const subject = await input
+    const output = subject.filter(e => e.type === 'unknown')
 
-    return output.should.eventually.have.length(0)
+    output.should.have.length(0)
   })
 })
