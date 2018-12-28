f0c95fea602ac2d0a6555d85095d31375d3b77fa
Filip Lindqvist <buggfille@gmail.com>
2018-12-28 12:37:55 +0100

    Style fixes

--- functions/debug/export.js
+++ functions/debug/export.js
@@ -1,17 +1,17 @@
-const secrets = require('../../.secrets.json');
-const dumper = require('../lib/dumper');
-const admin = require("firebase-admin");
-const path = require('path');
+const secrets = require('../../.secrets.json')
+const dumper = require('../lib/dumper')
+const admin = require('firebase-admin')
+const path = require('path')
 
 admin.initializeApp({
-	credential: admin.credential.cert(require(secrets.defaultCredentials)),
-	databaseURL: secrets.databaseURL
-});
+  credential: admin.credential.cert(require(secrets.defaultCredentials)),
+  databaseURL: secrets.databaseURL
+})
 
-const database = admin.firestore();
-const save = dumper.dump(database, path.join(__dirname, "../../backups"))
+const database = admin.firestore()
+const save = dumper.dump(database, path.join(__dirname, '../../backups'))
 
-save("events")
-save("versions")
-save("images")
-save("band_metadata")
+save('events')
+save('versions')
+save('images')
+save('band_metadata')
