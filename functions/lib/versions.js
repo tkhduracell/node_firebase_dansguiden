87ddb3b8e78bbb3583a4bb09a9dadc20e514d0a9
Filip Lindqvist <buggfille@gmail.com>
2019-01-13 22:51:13 +0100

    Clean test

--- functions/lib/versions.js
+++ functions/lib/versions.js
@@ -1,29 +1,29 @@
 const scraperjs = require('scraperjs')
 
+const extractContent = module.exports.extractContent = $ => {
+  return {
+    lines: $("div:contains('What's New') > h2")
+      .parent()
+      .parent()
+      .find('content')
+      .get()
+      .map(itm => $(itm).text()
+        .replace(/^\W*\*\W*/, '')
+      )
+      .filter(s => s !== 'Read more'),
+    name: $("div:contains('Current Version') + span")
+      .get()
+      .map(itm => $(itm).text().trim())
+      .join(', '),
+    date: $("div:contains('Updated') + span")
+      .get()
+      .map(itm => $(itm).text().trim())
+      .join(', ')
+  }
+}
+
 module.exports.getLatest = (log) => {
   const url = 'https://play.google.com/store/apps/details?id=feality.dans'
-
-  const extractContent = ($) => {
-    return {
-      lines: $("div:contains('What's New') > h2")
-        .parent()
-        .parent()
-        .find('content')
-        .get()
-        .map(itm => $(itm).text()
-          .replace(/^\W*\*\W*/, '')
-        )
-        .filter(s => s !== 'Read more'),
-      name: $("div:contains('Current Version') + span")
-        .get()
-        .map(itm => $(itm).text().trim())
-        .join(', '),
-      date: $("div:contains('Updated') + span")
-        .get()
-        .map(itm => $(itm).text().trim())
-        .join(', ')
-    }
-  }
 
   log('Running Google Play parser...')
   return new Promise((resolve, reject) => {
--- functions/lib/versions.test.js
+++ functions/lib/versions.test.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 const versions = require('./versions')
+const cheerio = require('cheerio')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
@@ -9,17 +10,45 @@ chai.should()
 // node_modules/.bin/mocha --reporter spec functions/lib/versions.test.js
 
 describe('versions', () => {
-  const version = versions.getLatest(() => {})
+
+  const $ = cheerio.load(`
+    <html>
+      <h1>Page</h1>
+      <div>
+        <div>
+          <h2>What's New</h2>
+        </div>
+        <content>* Foobar</content>
+      </div>
+      <div>
+        <div>Current Version</div>
+          <span>
+            <div>
+              <span>2.2.23</span>
+            </div>
+          </span>
+        </div>
+        <div>Updated</div>
+          <span>
+            <div>
+              <span>August 13, 2018</span>
+            </div>
+          </span>
+        </div>
+      </div>
+    </html>
+  `)
+  const version = versions.extractContent($)
 
   it('should return none empty version', async () => {
-    return (await version).name.should.not.be.empty
+    return (await version).name.should.be.equal('2.2.23')
   })
 
   it('should return none empty lines', async () => {
-    return (await version).lines.should.not.be.empty
+    return (await version).lines.should.be.eql(['Foobar'])
   })
 
   it('should return none empty date', async () => {
-    return (await version).date.should.not.be.empty
+    return (await version).date.should.be.equal('August 13, 2018')
   })
 })
