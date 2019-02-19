13753f2b43e95397cfec75c23573b0b5b8b14f08
Filip Lindqvist <buggfille@gmail.com>
2019-02-19 22:46:16 +0100

    Update debug_events file

--- functions/debug/debug_events.js
+++ functions/debug/debug_events.js
@@ -1,8 +1,11 @@
-const fs = require('fs')
 const events = require('../lib/events')
 
-events.update(console.log).then((res) => {
-  console.log('result')
-  console.log(JSON.stringify(res, undefined, 2))
-  fs.writeFileSync('output.json', JSON.stringify(res, undefined, 2))
-})
+events.parse(console.log)
+  .then((res) => {
+    console.log('------------------------------------')
+    res.filter(e => e.date === '2019-03-06')
+      .forEach(e => {
+        console.log(JSON.stringify(e, undefined, 2))
+      })
+  })
+  .catch(console.error)
