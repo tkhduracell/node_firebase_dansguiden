d953c7e780a1e73d52a6fb79bec4e1892b07feed
Filip Lindqvist <buggfille@gmail.com>
2024-08-20 19:19:05 +0200

    Update and sort overrides

--- functions/src/lib/spotify/artists.ts
+++ functions/src/lib/spotify/artists.ts
@@ -19,40 +19,42 @@ const commonGenres = [
 ]
 
 const blacklist = [
-  '42dnStXXTO4OXleuxwGL9F', // Andreas J
-  '4t56SOthZtGQkZdgZrAeqK', // Allstars,
-  '0MzvueasOOSFOrMKdkKb9C', // Bagatelle,
-  '3zo2TkHcXi6iXQxp4bipYW', // Bendix
-  '3h7VkDi0XjJMAtgZS4chp7', // Bendix
-  '7Fhj0K08T75UchC1p8T6g1', // Barons
+  '0B114ZpJddB3jl8AHu4OKT', // Phix
   '0hYTsX1dRl01ss8jizhrJo', // Framed & Keko
   '0kbxBTkWdVjByBeVVzSckZ', // Jive (rulltrappor)
+  '0MzvueasOOSFOrMKdkKb9C', // Bagatelle,
+  '0XY9QKkTnONBkjszYTfsZl', // Cedrix
+  '2iGJ84wIQmPyqa0NYKAjty', // Christer Eklund
+  '2wpdrWJCZhr737YxVJh2fe', // Bendix
+  '36jTseDCcfNAXsplm6dHiT', // Glads,
+  '377JJnVNZmREVqD5qIe18J', // Streaks
+  '3CEF3A8IybbJcwFSUVtAYM', // Strike
+  '3h7VkDi0XjJMAtgZS4chp7', // Bendix
+  '3zHaWg01z30TzjlWqSpeB4', // Karl Erik
+  '3zo2TkHcXi6iXQxp4bipYW', // Bendix
+  '42dnStXXTO4OXleuxwGL9F', // Andreas J
+  '4AiqdizfguNQoWQ5N6Aaza', // C-Laget
+  '4C3XwtLJafxjFTvHLxUsKS', // Strike
+  '4t56SOthZtGQkZdgZrAeqK', // Allstars,
+  '4zYFHS8R8wSOhcIBEbwxwA', // Charlies
+  '56url7nLtPIXWu0PD6XhIw', // Freqvens
+  '5Aql8TJ5Kfjf9f0hjtXiKZ', // Framed
   '5ecCx0PnQGB6odt9F9XD46', // Caspers,
-  'https://open.spotify.com/artist/377JJnVNZmREVqD5qIe18J', // Streaks
-  'https://open.spotify.com/artist/4zYFHS8R8wSOhcIBEbwxwA', // Charlies
-  'https://open.spotify.com/artist/60rrONrFQipKsTXYazdmQO', // Remix,
-  'https://open.spotify.com/artist/3CEF3A8IybbJcwFSUVtAYM', // Strike
-  'https://open.spotify.com/artist/6zNO1D32YLKnbC87DYVVBx', // Junix
-  'https://open.spotify.com/artist/3zHaWg01z30TzjlWqSpeB4', // Karl Erik
-  'https://open.spotify.com/artist/5XZN5RKQkiN6hGofPCnpjK', // Serenad
-  'https://open.spotify.com/artist/5QmZNcdq0h1NuL1G7t2yGp', // Tempo
-  'https://open.spotify.com/artist/2wpdrWJCZhr737YxVJh2fe', // Bendix
-  'https://open.spotify.com/artist/0XY9QKkTnONBkjszYTfsZl', // Cedrix
-  'https://open.spotify.com/artist/56url7nLtPIXWu0PD6XhIw', // Freqvens
-  'https://open.spotify.com/artist/5Aql8TJ5Kfjf9f0hjtXiKZ', // Framed
-  'https://open.spotify.com/artist/4AiqdizfguNQoWQ5N6Aaza', // C-Laget
-  'https://open.spotify.com/artist/36jTseDCcfNAXsplm6dHiT', // Glads,
-  'https://open.spotify.com/artist/0B114ZpJddB3jl8AHu4OKT', // Phix
-  'https://open.spotify.com/artist/2iGJ84wIQmPyqa0NYKAjty', // Christer Eklund
+  '5QmZNcdq0h1NuL1G7t2yGp', // Tempo
+  '5XZN5RKQkiN6hGofPCnpjK', // Serenad
+  '60rrONrFQipKsTXYazdmQO', // Remix,
+  '6zNO1D32YLKnbC87DYVVBx', // Junix
+  '7Fhj0K08T75UchC1p8T6g1', // Barons
 ].map(linkToId)
 
-const exact = _.mapValues({
+const promotedCandidates = _.mapValues({
   'Holidays': '6IxjXsaqZ81Fi1HiwRq4LS',
   'Eloge': '3rOpdjTyTjMpBDQXWClnav',
-  'Framed': 'https://open.spotify.com/artist/5vDnmN0QFphRUc5mNfPjtf?si=SYBL_F4-Qe-GNERFUmmLBQ'
+  'Framed': '5vDnmN0QFphRUc5mNfPjtf'
 } as Record<string, string>, linkToId)
 
 const remapping = {
+  'Eloge': 'Eloge Orkester',
   'PerHåkans': 'Per-Håkans',
   'PHs': 'Per-Håkans',
   'Larz Kristers': 'Larz-Kristers',
@@ -88,7 +90,7 @@ function isNotBlacklisted(a: {  id:  string }): boolean {
 }
 
 function isWhitelisted(a: { id: string }, target: string): boolean {
-  return exact[target] === a.id
+  return promotedCandidates[target] === a.id
 }
 
 function score (a: SpotifyApi.ArtistObjectFull, target: string): number {
