548677ea1e6a205b8a48fc0e573e0afc48da690f
Filip Lindqvist <buggfille@gmail.com>
2024-12-08 22:57:17 +0100

    Allow local files

--- functions/scripts/image_upload.ts
+++ functions/scripts/image_upload.ts
@@ -8,27 +8,40 @@ admin.initializeApp({
 })
 
 import { createInterface } from 'node:readline/promises'
+import { readFile } from 'node:fs/promises'
 (async () => {
-    
+
     const rl = createInterface({ input: process.stdin, output: process.stdout })
-    
+
     const band = await rl.question('Name of band: ')
-    
+
     const url = await rl.question('Image URL: ')
-    
-    try { new URL(url) } catch (e) {
-        console.error('Invalid URL', e)
-        process.exit(1)
-    }
-    
-    // 2. download the image
-    const resp = await fetch(url)
-    if (!resp.ok) {
-        console.error('Failed to download image', resp)
-        process.exit(1)
+
+    let buffer: ArrayBuffer
+    if (url.startsWith('http') && url.includes('://')) {
+        try { new URL(url) } catch (e) {
+            console.error('Invalid URL', e)
+            process.exit(1)
+        }
+
+        // 2. download the image
+        const resp = await fetch(url)
+        if (!resp.ok) {
+            console.error('Failed to download image', resp)
+            process.exit(1)
+        }
+
+        buffer = await resp.arrayBuffer()
+    } else {
+        try {
+            buffer = await readFile(url)
+        } catch (e) {
+            console.error('Invalid file', e)
+            process.exit(1)
+        }
     }
 
-    const buffer = await resp.arrayBuffer()
+
     const original = await sharp.default(buffer)
         .webp()
         .toBuffer()
@@ -38,9 +51,9 @@ import { createInterface } from 'node:readline/promises'
         .resize(300, 300, { fit: 'cover' })
         .webp()
         .toBuffer()
-    
+
     // 4. upload resized+original to firestore cloud storage /media
-    
+
     const dir = band.replace(/[^a-z0-9]/gi, '_')
         .replace(/å/gi, 'a')
         .replace(/ä/gi, 'a')
@@ -50,7 +63,7 @@ import { createInterface } from 'node:readline/promises'
     await largeFile.save(Buffer.from(original))
     await largeFile.makePublic()
     const largeFileUrl = largeFile.publicUrl()
-    
+
     const smallFile = admin.storage().bucket().file(`media/${dir}/small.webp`)
     await smallFile.save(Buffer.from(resized))
     await smallFile.makePublic()
