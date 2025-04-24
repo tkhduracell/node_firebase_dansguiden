09b56ca1b59c26dc6cacdd4ec57aa98ccd656cb4
Filip Lindqvist <buggfille@gmail.com>
2025-04-24 09:29:46 +0200

    fix: refactor error reporting initialization and ensure proper handling in production

--- functions/src/setup.ts
+++ functions/src/setup.ts
@@ -1,6 +1,13 @@
 // Source map support
 import 'source-map-support/register'
-import { ErrorReporting } from  '@google-cloud/error-reporting'
 
-const errorReporting = new ErrorReporting()
+// Error reporting
+import { ErrorReporting } from '@google-cloud/error-reporting'
+
+let errorReporting: ErrorReporting = (() => { }) as unknown as ErrorReporting
+
+if (process.env.NODE_ENV === 'production') {
+    errorReporting = new ErrorReporting()
+}
+
 export { errorReporting }
